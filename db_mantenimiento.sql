b2dcf83e7f2bc6db49676fb3e99b8483c37e2cbb
FerMedina18 <fernando2013mdina@gmail.com>
2020-11-19 02:00:57 -0600

    Ya se modifico la tabla Pieza y se agrego una nueva tabla

--- db_mantenimiento.sql
+++ db_mantenimiento.sql
@@ -1,14 +1,18 @@
+Use master;
+
+go
+
+Drop database db_mantenimiento;
+
+go
+
 Create database db_mantenimiento;
 
 go
 
 use db_mantenimiento;
 
-sp_addlogin 'usuario', '01234567'
-sp_addsrvrolemember 'usuario', 'sysadmin'
-
-sp_adduser 'usuario', 'usuario'
-sp_addrolemember 'db_accessadmin', 'usuario'
+go
 
 Create table Empleado(
 ID_Empleado int not null primary key identity(0, 1),
@@ -20,10 +24,6 @@ Cedula varchar(20) not null unique
 );
 
 go
-
-CREATE PROCEDURE VerEmpleados
-As
-SELECT * from Empleado
 
 Create table Equipo(
 ID_Equipo int not null primary key identity(0, 1),
@@ -83,6 +83,7 @@ go
 Create table Pieza(
 ID_Pieza int not null primary key identity(0, 1),
 Nombre varchar(25) not null,
+TiempoFallo int not null,
 Costo float not null,
 Descripcion varchar(100)
 );
@@ -107,16 +108,24 @@ Descripcion varchar(100)
 
 go
 
+Create table FalloEquipoMto(
+ID_FalloEqMto int not null primary key identity(0, 1),
+ID_FalloEquipo int not null,
+ID_Mantenimiento int not null,
+Foreign Key (ID_FalloEquipo) References FalloEquipo(ID_FalloEquipo),
+Foreign Key (ID_Mantenimiento) References Mantenimiento(ID_Mantenimiento)
+);
+
+go
+
 Create table OrdenMantenimiento(
 ID_OrdenMto int not null primary key identity(0, 1),
-ID_Mantenimiento int not null,
-ID_FalloEquipo int not null,
+ID_FalloEqMto int not null,
 Costo float not null,
 FechaRegistro date not null,
 Duracion time not null,
 Descripcion varchar(100),
-Foreign Key (ID_Mantenimiento) References Mantenimiento(ID_Mantenimiento),
-Foreign Key (ID_FalloEquipo) References FalloEquipo(ID_FalloEquipo)
+Foreign Key (ID_FalloEqMto) References FalloEquipoMto(ID_FalloEqMto)
 );
 
 go
@@ -135,7 +144,7 @@ insert into Fallo values('Fuga en la válula de gas');
 go
 insert into FalloEquipo values(0, 0, 750.00, 'Fuga');
 go
-insert into Pieza values('Válvula de gas', 450.00, 'VÁLVULA DE CONEXIÓN PARA CILINDROS DE GAS PROPANO A ESTUFA O COCINA.');
+insert into Pieza values('Válvula de gas', 2, 450.00, 'VÁLVULA DE CONEXIÓN PARA CILINDROS DE GAS PROPANO A ESTUFA O COCINA.');
 go 
 insert into PiezaFalloEq values(0, 0);
 go
@@ -145,4 +154,22 @@ insert into Mantenimiento values('Correctivo', 'Corrección de los defectos obser
 go
 insert into Mantenimiento values('Predictivo', 'Detección de posibles fallas cada cierto tiempo.');
 go
-insert into OrdenMantenimiento values(0, 0, 1200.00, '2020-11-17', '01:00:00.0000000', 'Se cambió la válvula de gas de la cocina.');
+insert into FalloEquipoMto values(0, 0);
+go
+insert into OrdenMantenimiento values(0, 1200.00, '2020-11-17', '01:00:00.0000000', 'Se cambió la válvula de gas de la cocina.');
+
+go
+
+sp_addlogin 'usuario', '01234567'
+go
+sp_addsrvrolemember 'usuario', 'sysadmin'
+go
+sp_adduser 'usuario', 'usuario'
+go
+sp_addrolemember 'db_accessadmin', 'usuario'
+
+go
+
+CREATE PROCEDURE VerEmpleados
+As
+SELECT * from Empleado
